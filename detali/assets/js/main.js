43cb3ec5bf8d525ff7771ba2be7c04378b549ded
Maxim <35780153+Rattlhead@users.noreply.github.com>
2018-04-19 14:16:05 +0300

    Добавил сайт

---
+++ detali/assets/js/main.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function() {
+ $('#btn').click(function (){$('.numbers-item__number').spincrement();});
+
+       // Начало скрипты меню
+       $(".top-nav__btn-menu").on('click', function() {
+         $(".top-nav__menu").toggleClass("d-none");
+
+       });
+       // Конец скрипта меню
+
+ // Начало Скрол до элемента
+       $("a").click(function() {
+         var elementClick = $(this).attr("href");
+         var destination = $(elementClick).offset().top;
+         $('html').animate({ scrollTop: destination }, 500);
+         $('body').animate({ scrollTop: destination }, 500);
+         return false;
+       });
+       // Конец Скрол до элемента
+
+// Начало анимация цифр
+
+ // var show = true;
+ //    var countbox = ".numbers-item__number";
+ //    $(window).on("scroll load resize", function () {
+ //        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+ //        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+ //        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+ //        var w_height = $(window).height(); // Высота окна браузера
+ //        var d_height = $(document).height(); // Высота всего документа
+ //        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+ //        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+ //            $('.numbers-item__number').css('opacity', '1');
+ //            $('.numbers-item__number').spincrement({
+ //                thousandSeparator: "",
+ //                duration: 1200
+ //            });
+
+ //            show = false;
+ //        }
+ //    });
+
+// Конец анимация цифр
+
+
+
+
+     });
+
