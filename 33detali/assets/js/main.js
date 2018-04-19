aea197729eabbbdaa121ced428f23be8aafffd01
Maxim <35780153+Rattlhead@users.noreply.github.com>
2018-04-19 17:53:51 +0300

    Обновил

--- 33detali/assets/js/main.js
+++ 33detali/assets/js/main.js
@@ -1,50 +1,78 @@
-
 $(document).ready(function() {
- $('#btn').click(function (){$('.numbers-item__number').spincrement();});
 
-       // Начало скрипты меню
-       $(".top-nav__btn-menu").on('click', function() {
-         $(".top-nav__menu").toggleClass("d-none");
+  wow = new WOW({
+    offset: 100, // default
+    mobile: false, // default
+    live: true // default
+  })
+  wow.init();
 
-       });
-       // Конец скрипта меню
+  // Начало анимация чисел
+  var show = true;
+  var countbox = ".numbers-item__number";
+  $(window).on("scroll load resize", function() {
+    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+    var w_height = $(window).height(); // Высота окна браузера
+    var d_height = $(document).height(); // Высота всего документа
+    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+    if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
 
- // Начало Скрол до элемента
-       $("a").click(function() {
-         var elementClick = $(this).attr("href");
-         var destination = $(elementClick).offset().top;
-         $('html').animate({ scrollTop: destination }, 500);
-         $('body').animate({ scrollTop: destination }, 500);
-         return false;
-       });
-       // Конец Скрол до элемента
-
-// Начало анимация цифр
-
- // var show = true;
- //    var countbox = ".numbers-item__number";
- //    $(window).on("scroll load resize", function () {
- //        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
- //        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
- //        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
- //        var w_height = $(window).height(); // Высота окна браузера
- //        var d_height = $(document).height(); // Высота всего документа
- //        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
- //        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
- //            $('.numbers-item__number').css('opacity', '1');
- //            $('.numbers-item__number').spincrement({
- //                thousandSeparator: "",
- //                duration: 1200
- //            });
-
- //            show = false;
- //        }
- //    });
-
-// Конец анимация цифр
+      $(countbox).spincrement({
+        duration: 2000,
+        complete: function(e) {
+          e.text(e.text() + 'k+')
+        }
+      });
+      show = false;
+    }
+  });
+  // Конец анимация чисел
 
 
+  // Начало скрипты меню
+  $(".top-nav__btn-menu").on('click', function() {
+    $(".top-nav__menu").toggleClass("d-none");
+
+  });
+  // Конец скрипта меню
+
+  // Начало Скрол до элемента
+  $("a").click(function() {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    $('html').animate({ scrollTop: destination }, 500);
+    $('body').animate({ scrollTop: destination }, 500);
+    return false;
+  });
+  // Конец Скрол до элемента
+
+  // Начало анимация цифр
+
+  // var show = true;
+  //    var countbox = ".numbers-item__number";
+  //    $(window).on("scroll load resize", function () {
+  //        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+  //        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+  //        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+  //        var w_height = $(window).height(); // Высота окна браузера
+  //        var d_height = $(document).height(); // Высота всего документа
+  //        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+  //        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+  //            $('.numbers-item__number').css('opacity', '1');
+  //            $('.numbers-item__number').spincrement({
+  //                thousandSeparator: "",
+  //                duration: 1200
+  //            });
+
+  //            show = false;
+  //        }
+  //    });
+
+  // Конец анимация цифр
 
 
-     });
 
+
+});
